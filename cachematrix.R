a56b983bcfd872dfc79faca736645d6dc72acf20
bl4r <bl4r@users.noreply.github.com>
2014-04-23 16:13:17 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,10 @@
-## This file contains two functions.  The first one, makeCacheMatrix, stores a matrix, enabling the second function, cacheSolve, to find and display the matrix's inverse.
+## This file contains two functions.
+## The first one, makeCacheMatrix, stores a matrix, enabling the second function,
+## cacheSolve, to find and display the matrix's inverse.
 
-## makeCacheMatrix requires only a matrix, x.  It's primary purpose is to store the information required for cacheSolve to run properly.  Note that the function does not test to make sure that the matrix provided is, in fact, invertible.
+## makeCacheMatrix requires only a matrix, x.  
+## It's primary purpose is to store the information required for cacheSolve to run properly.
+## Note that the function does not test to make sure that the matrix provided is, in fact, invertible.
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
@@ -20,7 +24,16 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## cacheSolve searches for the inverse of the matrix x.  If the inverse has already been calculated, cacheSolve indicates this to the user by printing a message and then displays the solution.  If the inverse has not been stored already, the function finds it using the solve function and then prints the solution.  cacheSolve requires makeCahceMatrix to already have been run.  this can be done by running the first function seperately and saving the results in memory or by nesting the functions.
+## cacheSolve searches for the inverse of the matrix x.
+## If the inverse has already been calculated, cacheSolve indicates this to the user
+## by printing a message and then displays the solution.
+## If the inverse has not been stored already, the function finds it using the solve
+## function and then prints the solution.
+
+## cacheSolve requires makeCahceMatrix to already have been run.  This can be done by
+## running the first function seperately and saving the results in memory or by nesting the functions.
+
+## Like makeCacheMatrix, cacheSolve does not test if the matrix is invertible before trying to solve it.
 
 cacheSolve <- function(x, ...) {
         m <- x$get_solve()
